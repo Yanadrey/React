bf2575aad1bb99e757bc7633ec4e401ffce9ac08
Yanadrey <123734623+Yanadrey@users.noreply.github.com>
2024-04-24 22:46:59 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text="Привет, мир!" />
+    </div>
+  );
+}
+
+export default App;
